e53b9ba319321b9c2ac49a5dab1955309cd2815d
QT_BESAU <quoctinhstudy1911@gmail.com>
2026-02-20 13:14:40 +0700

    clean repo remove cache files

--- addons/ql_kho_mypham/models/category.py
+++ addons/ql_kho_mypham/models/category.py
@@ -5,5 +5,6 @@ class mp_category(osv.osv):
     _name = 'mp.category'
     _columns = {
         'name': fields.char('Ten danh muc', required=True),
+        'description': fields.text('Mo ta'),
     }
 mp_category()
